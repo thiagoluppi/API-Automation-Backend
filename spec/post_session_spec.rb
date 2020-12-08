3b50221d1317301d81a888b9ea3acea614671e7e
Thiago M. Luppi <thiago.luppi@icloud.com>
2020-12-08 11:39:42 -0300

    commitando contextos do RSpec

--- spec/post_session_spec.rb
+++ spec/post_session_spec.rb
@@ -1,18 +1,25 @@
 require "httparty"
 
 describe "POST /sessions" do
-  it "login com sucesso" do
-    payload = { email: "thiago@icloud.com", password: "pwd123" }
+  context "login com sucesso" do
+    before (:all) do
+      payload = { email: "thiago@icloud.com", password: "pwd123" }
 
-    result = HTTParty.post(
-      "http://rocklov-api:3333/sessions",
-      body: payload.to_json,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    )
+      @result = HTTParty.post(
+        "http://rocklov-api:3333/sessions",
+        body: payload.to_json,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      )
+    end
 
-    expect(result.code).to eql 200
-    expect(result.parsed_response["_id"].length).to eql 24
+    it "valida status code" do
+      expect(@result.code).to eql 200
+    end
+
+    it "valida id do usuario" do
+      expect(@result.parsed_response["_id"].length).to eql 24
+    end
   end
 end
