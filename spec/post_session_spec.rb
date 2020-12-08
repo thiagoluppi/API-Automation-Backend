031fa0d54e306c0d88e8831300be296c3a5cb62b
Thiago M. Luppi <thiago.luppi@icloud.com>
2020-12-08 16:47:52 -0300

    commitando teste orientado a dados

--- spec/post_session_spec.rb
+++ spec/post_session_spec.rb
@@ -19,18 +19,58 @@ describe "POST /sessions" do
   #################################################################
   #################################################################
 
-  context "senha invalida" do
-    before (:all) do
-      payload = { email: "thiago@icloud.com", password: "123456" }
-      @result = Sessions.new.login(payload)
-    end
+  examples = [
+    {
+      title: "senha incorreta",
+      payload: { email: "thiago@icloud.com", password: "123456" },
+      code: 401,
+      error: "Unauthorized",
+    },
+    {
+      title: "usuario nao existe",
+      payload: { email: "404@icloud.com", password: "pwd123" },
+      code: 401,
+      error: "Unauthorized",
+    },
+    {
+      title: "email em branco",
+      payload: { email: "", password: "pwd123" },
+      code: 412,
+      error: "required email",
+    },
+    {
+      title: "sem o campo email",
+      payload: { password: "pwd123" },
+      code: 412,
+      error: "required email",
+    },
+    {
+      title: "senha em branco",
+      payload: { email: "thiago@icloud.com", password: "" },
+      code: 412,
+      error: "required password",
+    },
+    {
+      title: "sem o campo senha",
+      payload: { email: "thiago@icloud.com" },
+      code: 412,
+      error: "required password",
+    },
+  ]
 
-    it "valida status code" do
-      expect(@result.code).to eql 401
-    end
+  examples.each do |e|
+    context "#{e[:title]}" do
+      before (:all) do
+        @result = Sessions.new.login(e[:payload])
+      end
 
-    it "valida resposta requisicao" do
-      expect(@result.parsed_response["error"]).to eql "Unauthorized"
+      it "valida status code #{e[:code]}" do
+        expect(@result.code).to eql e[:code]
+      end
+
+      it "valida mensagem de erro" do
+        expect(@result.parsed_response["error"]).to eql e[:error]
+      end
     end
   end
 end
